75ffc091d81aaa7569783ddc3781e10ef98124b1
Thainahelliza <84358089+Thainahelliza@users.noreply.github.com>
2021-07-19 17:11:32 -0300

    Ajustes finais

--- src/main/ProjetoJavamos/com/javamos/projeto/modelview/HomeView.java
+++ src/main/ProjetoJavamos/com/javamos/projeto/modelview/HomeView.java
@@ -5,16 +5,26 @@ import java.util.List;
 
 public class HomeView {
 	public String getMensagem () {
-		return "Bem vindo";
+		return "Bem-vindo";
 	}
 	
 	public List<String> getEndpoints(){
 		var lista = new ArrayList<String>();
 		lista.add("/contas");
-		lista.add("/contas/{id}");
+		lista.add("/conta/1");
+		lista.add("/conta/2");
+		lista.add("/conta/3");
+		lista.add("/conta/4");
+		lista.add("/conta/5");
+		lista.add("/conta/6");
+		lista.add("/conta/7");
+						
+		lista.add("");
 		
-		lista.add("/cliente");
-		lista.add("/cliente/{id}");
+		lista.add("/clientes");
+		lista.add("/cliente/1");
+		lista.add("/cliente/2");
+		lista.add("/cliente/3");
 		
 		return lista;
 		
--- src/main/ProjetoJavamos/com/javamos/projeto/repo/ContaRepo.java
+++ src/main/ProjetoJavamos/com/javamos/projeto/repo/ContaRepo.java
@@ -5,5 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 import com.javamos.projeto.model.Conta;
 
 public interface ContaRepo extends CrudRepository <Conta, Integer> {
-
 }
